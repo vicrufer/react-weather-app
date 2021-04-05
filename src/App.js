f93a7ec82d3348ec808b9bdf6eef16fd480aed06
vicrufer <76908321+vicrufer@users.noreply.github.com>
2021-04-05 15:07:45 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        Weather App
-      </h1>
-      <footer>
-        This project was coded by Victoria Ruiz and it's {""} 
-        <a href="https://github.com/vicrufer/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>
+          Weather App
+        </h1>
+        <footer>
+          This project was coded by Victoria Ruiz and it's {""} 
+          <a href="https://github.com/vicrufer/react-weather-app" target="_blank" rel="noreferrer">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
