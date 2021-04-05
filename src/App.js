9153597331837cc9de989b266976ad5f5f86b347
vicrufer <76908321+vicrufer@users.noreply.github.com>
2021-04-05 12:03:20 +0200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>
+        Weather App
+      </h1>
+      <footer>
+        This project was coded by Victoria Ruiz and it's {""} 
+        <a href="https://github.com/vicrufer/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
