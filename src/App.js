1b3b3727d06e632f0374b86319cf799a81dfe06d
vicrufer <76908321+vicrufer@users.noreply.github.com>
2021-04-06 13:19:21 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 
 import './App.css';
 
@@ -6,9 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>
-          Weather App
-        </h1>
+        <Weather />
         <footer>
           This project was coded by Victoria Ruiz and it's {""} 
           <a href="https://github.com/vicrufer/react-weather-app" target="_blank" rel="noreferrer">
