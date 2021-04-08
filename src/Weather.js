127bd2f207226d7d5c0ea7edeae50b962236d9cb
vicrufer <76908321+vicrufer@users.noreply.github.com>
2021-04-08 19:24:27 +0200

    Added weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
-            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            icon: response.data.weather[0].icon,
             wind: response.data.wind.speed,
             city: response.data.name
         });
