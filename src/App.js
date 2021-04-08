abe516c54b213162db6fc2eb8dc346ffaf7b0a5c
vicrufer <76908321+vicrufer@users.noreply.github.com>
2021-04-08 13:16:29 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Victoria Ruiz and it's {""} 
           <a href="https://github.com/vicrufer/react-weather-app" target="_blank" rel="noreferrer">
